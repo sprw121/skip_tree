0ab3b56e100651a16d5669f64da5504371727b7f
Steven Wright <steven.wright@adroll.com>
2015-05-25 23:42:01 -0700

    Use preferred braces style

--- skip_tree.c
+++ skip_tree.c
@@ -14,7 +14,8 @@ int contains(int key)
 
 static inline int is_subtree_root(skip_tree_node* node)
 {
-    if(node == NULL) {
+    if(node == NULL)
+    {
         return 1;
     }
 
@@ -23,7 +24,8 @@ static inline int is_subtree_root(skip_tree_node* node)
 
 static inline int stn_ptr_to_int(skip_tree_node* node)
 {
-    if(node == NULL) {
+    if(node == NULL)
+    {
         return -1;
     }
 
@@ -32,7 +34,8 @@ static inline int stn_ptr_to_int(skip_tree_node* node)
 
 void print_node(int key)
 {
-    if(s_root == NULL) {
+    if(s_root == NULL)
+    {
         printf("Uninitialized tree\n\n");
         return;
     }
@@ -50,7 +53,8 @@ static inline int flip(int i)
 
 static void __print_stree(skip_tree_node* node, int level, int* print_branchs)
 {
-    if(node == NULL) {
+    if(node == NULL)
+    {
         return;
     }
 
@@ -62,14 +66,19 @@ static void __print_stree(skip_tree_node* node, int level, int* print_branchs)
 
     print_branchs[level] = flip(print_branchs[level]);
 
-    if(level != 0) {
+    if(level != 0)
+    {
         printf("    ");
     }
 
-    for(int i = 1; i < level; i++) {
-        if(print_branchs[i]) {
+    for(int i = 1; i < level; i++)
+    {
+        if(print_branchs[i])
+        {
             printf("|   ");
-        } else{
+        }
+        else
+        {
             printf("    ");
         }
     }
@@ -85,7 +94,8 @@ void print_stree(void)
 {
     printf("\n\n---------------------------------\n\n");
 
-    if(s_root == NULL) {
+    if(s_root == NULL)
+    {
         printf("Uninitialized tree\n\n");
         return;
     }
@@ -99,7 +109,8 @@ void print_stree(void)
 
 static void __print_dtree(skip_tree_node* node, int level, int* print_branchs)
 {
-    if(node == NULL) {
+    if(node == NULL)
+    {
         return;
     }
 
@@ -111,14 +122,19 @@ static void __print_dtree(skip_tree_node* node, int level, int* print_branchs)
 
     print_branchs[level] = flip(print_branchs[level]);
 
-    if(level != 0) {
+    if(level != 0)
+    {
         printf("    ");
     }
 
-    for(int i = 1; i < level; i++) {
-        if(print_branchs[i] == 1) {
+    for(int i = 1; i < level; i++)
+    {
+        if(print_branchs[i] == 1)
+        {
             printf("|   ");
-        } else{
+        }
+        else
+        {
             printf("    ");
         }
     }
@@ -134,12 +150,14 @@ void print_dtree(void)
 {
     printf("\n\n---------------------------------\n\n");
 
-    if(s_root == NULL) {
+    if(s_root == NULL)
+    {
         printf("Uninitialized tree\n\n");
         return;
     }
 
-    if(d_root == NULL) {
+    if(d_root == NULL)
+    {
         printf("Empty tree\n\n");
         return;
     }
@@ -155,63 +173,82 @@ void insert(int key, void * data)
 {
     printf("Inserting %d\n", key);
 
-    if(s_root == NULL) {
+    if(s_root == NULL)
+    {
         printf("Uninitialized tree\n\n");
         return;
     }
 
-    if(s[key].data) {
+    if(s[key].data)
+    {
         printf("Node %d occupied\n", key);
     }
 
-    if(d_root == NULL || s[key].d_lchild == d_root || s[key].d_rchild == d_root) {
+    if(d_root == NULL || s[key].d_lchild == d_root || s[key].d_rchild == d_root)
+    {
         d_root = s + key;
     }
 
     s[key].data = data;
     skip_tree_node * child = s + key, *current = s + key;
 
-    if(is_subtree_root(current)) {
+    if(is_subtree_root(current))
+    {
         return;
     }
 
-    if(current->d_rchild != NULL) {
+    if(current->d_rchild != NULL)
+    {
         (current->d_rchild)->d_par = current;
     }
 
-    if(current->d_lchild != NULL) {
+    if(current->d_lchild != NULL)
+    {
         (current->d_lchild)->d_par = current;
     }
 
-    while(1) {
+    while(1)
+    {
         skip_tree_node* last = current;
         current = current->s_par;
 
-        if(current == NULL) {
+        if(current == NULL)
+        {
             child->d_par = current;
             break;
-        } else if(is_subtree_root(current) || current->data) {
-            if(current < last) {
+        }
+        else if(is_subtree_root(current) || current->data)
+        {
+            if(current < last)
+            {
                 current->d_rchild = child;
-            } else {
+            }
+            else {
                 current->d_lchild = child;
             }
 
             child->d_par = current;
             break;
-        } else {
-            if(current < last) {
+        }
+        else
+        {
+            if(current < last)
+            {
                 current->d_rchild = child;
-            } else {
+            }
+            else
+            {
                 current->d_lchild = child;
             }
 
-            if(is_subtree_root(current)) {
+            if(is_subtree_root(current))
+            {
                 (current->d_lchild)->d_par = current;
                 (current->d_rchild)->d_par = current;
                 child = current;
 
-                if(d_root == current->d_lchild || d_root == current->d_rchild) {
+                if(d_root == current->d_lchild || d_root == current->d_rchild)
+                {
                     d_root = current;
                 }
             }
@@ -228,62 +265,79 @@ void erase(int key)
 {
     printf("Erasing %d\n", key);
 
-    if(s_root == NULL) {
+    if(s_root == NULL)
+    {
         printf("Uninitialized tree\n\n");
         return;
     }
 
-    if(s[key].data == NULL) {
+    if(s[key].data == NULL)
+    {
         printf("Erase failed: Key %d does not exist\n", key);
         return;
     }
 
     s[key].data = NULL;
 
-    if(is_subtree_root(s + key)) {
+    if(is_subtree_root(s + key))
+    {
         return;
-    } else if (s[key].d_lchild != NULL) {
+    }
+    else if (s[key].d_lchild != NULL)
+    {
         (s[key].d_lchild)->d_par = s[key].d_par;
-    } else if (s[key].d_rchild != NULL) {
+    }
+    else if (s[key].d_rchild != NULL)
+    {
         (s[key].d_rchild)->d_par = s[key].d_par;
     }
 
     skip_tree_node* child = get_child(s + key), *current = s + key;
 
-    if(d_root == s + key) {
+    if(d_root == s + key)
+    {
         d_root = child;
     }
 
-    while(1) {
+    while(1)
+    {
         skip_tree_node* last = current;
         current = current->s_par;
 
-        if(current == NULL) {
+        if(current == NULL)
+        {
             return;
         }
 
         int was_subtree_root = is_subtree_root(current);
 
-        if(current > last) {
+        if(current > last)
+        {
             current->d_lchild = child;
-        } else {
+        }
+        else
+        {
             current->d_rchild = child;
         }
 
-        if(current->data != NULL) {
+        if(current->data != NULL)
+        {
             break;
         }
 
-        if(is_subtree_root(current)) {
+        if(is_subtree_root(current))
+        {
             break;
         }
 
-        if(was_subtree_root) {
+        if(was_subtree_root)
+        {
             child = get_child(current);
             child->d_par = current->d_par;
         }
 
-        if(current == d_root && current->data == NULL) {
+        if(current == d_root && current->data == NULL)
+        {
             d_root = get_child(current);
         }
     }
@@ -291,44 +345,54 @@ void erase(int key)
 
 int predecessor(int key)
 {
-    if(s_root == NULL) {
+    if(s_root == NULL)
+    {
         printf("Uninitialized tree\n\n");
         return -1;
     }
 
-    if(s[key].data == NULL) {
+    if(s[key].data == NULL)
+    {
         printf("Element with key %d has not yet been inserted\n", key);
     }
 
     skip_tree_node* current = s + key;
 
-    if(current->d_lchild != NULL) {
+    if(current->d_lchild != NULL)
+    {
         current = current->d_lchild;
 
-        while(current->d_rchild != NULL) {
+        while(current->d_rchild != NULL)
+        {
             current = current->d_rchild;
         }
 
         return stn_ptr_to_int(current);
-    } else {
+    }
+    else
+    {
         skip_tree_node* prev;
 
-        do {
+        do
+        {
             prev = current;
             current = current->d_par;
         } while(current != NULL && current > prev);
 
-        if(current == NULL) {
+        if(current == NULL)
+        {
             return -1;
         }
 
-        if(current->data) {
+        if(current->data)
+        {
             return stn_ptr_to_int(current);
         }
 
         current = current->d_lchild;
 
-        while(current->d_rchild != NULL) {
+        while(current->d_rchild != NULL)
+        {
             current = current->d_rchild;
         }
 
@@ -338,43 +402,53 @@ int predecessor(int key)
 
 int successor(int key)
 {
-    if(s_root == NULL) {
+    if(s_root == NULL)
+    {
         printf("Uninitialized tree\n\n");
         return -1;
     }
 
-    if(s[key].data == NULL) {
+    if(s[key].data == NULL)
+    {
         printf("Element with key %d has not yet been inserted\n", key);
     }
 
     skip_tree_node* current = s + key;
 
-    if(current->d_rchild != NULL) {
+    if(current->d_rchild != NULL)
+    {
         current= current->d_rchild;
-        while(current->d_lchild != NULL) {
+        while(current->d_lchild != NULL)
+        {
             current = current->d_lchild;
         }
 
         return stn_ptr_to_int(current);
-    } else {
+    }
+    else
+    {
         skip_tree_node* prev;
 
-        do {
+        do
+        {
             prev = current;
             current = current->d_par;
         } while(current != NULL && current < prev);
 
-        if(current == NULL) {
+        if(current == NULL)
+        {
             return -1;
         }
 
-        if(current->data) {
+        if(current->data)
+        {
             return stn_ptr_to_int(current);
         }
 
         current = current->d_rchild;
 
-        while(current->d_lchild != NULL) {
+        while(current->d_lchild != NULL)
+        {
             current = current->d_lchild;
         }
 
@@ -391,15 +465,21 @@ static skip_tree_node* set_structure(int left_bound, int right_bound, skip_tree_
     s[node].d_rchild = NULL;
     s[node].data = NULL;
 
-    if(left_bound != node) {
+    if(left_bound != node)
+    {
         s[node].s_lchild = set_structure(left_bound, node - 1, s + node);
-    } else {
+    }
+    else
+    {
         s[node].s_lchild = NULL;
     }
 
-    if(right_bound != node) {
+    if(right_bound != node)
+    {
         s[node].s_rchild = set_structure(node + 1, right_bound, s + node);
-    } else {
+    }
+    else
+    {
         s[node].s_rchild = NULL;
     }
 
@@ -408,7 +488,8 @@ static skip_tree_node* set_structure(int left_bound, int right_bound, skip_tree_
 
 int init(int length)
 {
-    if((s = malloc(length * sizeof(skip_tree_node))) == NULL) {
+    if((s = malloc(length * sizeof(skip_tree_node))) == NULL)
+    {
         return 1;
     }
 
@@ -422,12 +503,14 @@ int init(int length)
 
 void destroy(void)
 {
-    if(s_root == NULL) {
+    if(s_root == NULL)
+    {
         printf("Uninitialized tree\n\n");
         return;
     }
 
-    if(s) {
+    if(s)
+    {
         free(s);
     }
 
